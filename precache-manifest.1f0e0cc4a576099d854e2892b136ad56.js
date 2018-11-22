2519441a08198fc4cd98d018644e83a1d037989c
HiiamCam <tam.vir89@gmail.com>
2018-11-22 08:08:42 +0700

    Updates

---
+++ precache-manifest.1f0e0cc4a576099d854e2892b136ad56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3fcdd4822f495a6d5224",
+    "url": "/static/js/main.3fcdd482.chunk.js"
+  },
+  {
+    "revision": "3f956256f4a3ae2bb4a1",
+    "url": "/static/js/1.3f956256.chunk.js"
+  },
+  {
+    "revision": "3fcdd4822f495a6d5224",
+    "url": "/static/css/main.d12cdae6.chunk.css"
+  },
+  {
+    "revision": "1a384a39a6f3a5367e17a14f2476c8a3",
+    "url": "/index.html"
+  }
+];
